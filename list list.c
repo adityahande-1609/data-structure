a9ee261747548d3dbfd0f90afdf17f71aa408348
Aditya hande <hande.adu@gmail.com>
2026-08-14 00:56:01 +0530

    Refactor linked list operations and add delete functionality

--- list list.c
+++ list list.c
@@ -1,56 +1,69 @@
-#include <stdio.h>
-#include <stdlib.h>
-struct node
-{
-    int data;
-    struct node *next;
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+	int data;
+	struct node *next;
 };
-struct node *head;
-void insertbeg(void);
-void display(void);
-int main()
-{ int a;
-while (1)
-{
-    printf("1.Insert at beginning \n 2.Display \n 3.exit");
-    printf ("\nEnter your choice:");
-    scanf ("%d",&a);
-    switch(a)
-    {
-        case 1: insertbeg();
-        case 2: display();
-        case 3: exit(1);
+void create(){
+	node *n1;
+	n1=(struct node*)malloc(sizeof(struct node));
+	printf("enter data :");
+	scanf("%d",&n1->data);
+	n1->next=NULL;
+	if (head==NULL){
+		head=n1;
+	}
+	else{
+		n1->next=head;
+		head=n1;
+	}
+}
+void rem(){
+	if (head==NULL){
+	printf("\nlist empty\n");
+	}
+	else{
+	struct node *temp;
+	temp=head;
+	head=head->next;
+	free(temp);
+    }
+	
+}
+void disp(){
+	node *temp;
+	if (head==NULL){
+	printf("\nlist empty\n");
+	}
+	else{
+		temp=head;
+		printf("list items are : ");
+	while(temp!=NULL){
+	printf("%d ",temp->data);
+	temp=temp->next;
     }
 }
-    return 0;
 }
-  void insertbeg()
-    {
-        struct node *newnode;
-        newnode=(struct node*)malloc(sizeof(struct node));
-        printf ("Enter data:");
-        scanf ("%d", &newnode->data);
-        newnode->next=NULL;
-        if (head==NULL)
-        {
-            head=newnode;
-        }
-        else{
-        newnode->next = head;
-        head = newnode;
-        }
-    }
-  void display()
-  {
-      struct node*temp;
-      temp=head;
-      if (head==NULL)
-      {
-          printf ("Empty");
-      }
-      while (temp!=NULL)
-      {
-          printf ("%d", temp->data);
-          temp=temp->next;
-      }
-  }
+struct node *head=NULL;
+int main(){
+	int a;
+	while(1)
+	{
+		printf("\nenter your choice\n");
+		printf("1.insert in the begining\n2.delete from the begining \n3.display\n4.exit\n");
+		scanf("%d",&a);
+		switch(a){
+			case 1:
+				create();
+				break;
+			case 2:
+				rem();
+				break;
+			case 3:
+				disp();
+				break;
+			case 4:exit(1);
+		}
+	}
+	return 0;
+}
